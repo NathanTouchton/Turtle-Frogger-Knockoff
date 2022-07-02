855cf84f35d418874212b88cdd94f6196883b02a
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-07-02 15:56:12 -0500

    Add files via upload

--- Turtle Frogger Knockoff/main.py
+++ Turtle Frogger Knockoff/main.py
@@ -48,5 +48,5 @@ screen.exitonclick()
 # When this happens, return the turtle to the starting position and increase the speed of the cars. 
 # Hint: think about creating an attribute and using the MOVE_INCREMENT to increase the car speed. If you get stuck, check the video walkthrough in Step 6.
 
-# Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing,
-# the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
+# Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing,the level should increase.
+# When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
--- Turtle Frogger Knockoff/scoreboard.py
+++ Turtle Frogger Knockoff/scoreboard.py
@@ -1,7 +1,15 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.speed(0)
+        self.hideturtle()
+        self.write(arg="test", align="center", font=(FONT))
+        
 
-# Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing,
-# the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
+# Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing,the level should increase.
+# When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
